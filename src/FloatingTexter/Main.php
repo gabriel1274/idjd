3e00413e7773ddad42e78fad3ac7dc4045d0a409
Anthony Calabretta <tony0000.ac@gmail.com>
2016-02-18 16:56:55 +0100

    Wait a sec.. adding spaces

--- src/FloatingTexter/Main.php
+++ src/FloatingTexter/Main.php
@@ -12,13 +12,14 @@ use pocketmine\math\Vector3;
 class Main extends PluginBase implements Listener{
 
 	public function onEnable(){
+
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getLogger()->info(TextFormat::GREEN. "Plugin Enabled");
 
 		if(!is_dir($this->getDataFolder())){
 			@mkdir($this->getDataFolder());
 		}
-		
+
 		if(!file_exists($this->getDataFolder() . "config.yml")){
 			$this->saveDefaultConfig();
 		}
@@ -27,15 +28,19 @@ class Main extends PluginBase implements Listener{
 	}
 	
 	public function onDisable(){
-		$this->saveDefaultConfig();
+
 		$this->getLogger()->info(TextFormat::RED. "Plugin Disabled");
+
 	}
  
 	public function onPlayerJoin(PlayerJoinEvent $event){
+
 		foreach($this->cfg->get("floats") as $floats){
+
 			$level = $event->getPlayer()->getLevel();
 			$vect = new Vector3($floats["x"], $floats["y"], $floats["z"]);
 			$finaltext = "";
+
 			foreach($floats["text"] as $text){
 				$finaltext .= $text . "\n";
 			}
